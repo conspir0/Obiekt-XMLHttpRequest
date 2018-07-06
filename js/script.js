872d2e89c3b760fd542df8fd0e9af9b30bbb696f
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-06 13:52:52 +0200

    Add script in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+var url = 'http://api.icndb.com/jokes/random';
+
+// button
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function() {
+    getJoke();
+});
+
+// selector p - place to show random joke
+var paragraph = document.getElementById('joke');
+
+// implementation getJoke()
+function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function() {
+        var response = JSON.parse(xhr.response);
+        paragraph.innerHTML = response.value.joke;
+    });
+    xhr.send();
+}
+
+// initial joke
+getJoke();
